505ebda3805bd8475a3ebcb6065775c433c7ca6b
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-10-13 16:49:16 -0500

    completed the loops exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,16 +3,64 @@
 /* ########################################################################## */
 
 function showMultiplicationTable(num) {
-    console.log(num + " x 1 = " + num * 1);
-    console.log(num + " x 2 = " + num * 2);
-    console.log(num + " x 3 = " + num * 3);
-    console.log(num + " x 4 = " + num * 4);
-    console.log(num + " x 5 = " + num * 5);
-    console.log(num + " x 6 = " + num * 6);
-    console.log(num + " x 7 = " + num * 7);
-    console.log(num + " x 8 = " + num * 8);
-    console.log(num + " x 9 = " + num * 9);
-    console.log(num + " x 10 = " + num * 10);
+    for (let i = 1; i <= 10; i++) {
+        console.log(num + " x " + i + " = " + num * i);
+    }
 }
 
-console.log(showMultiplicationTable(7));
+// console.log(showMultiplicationTable(2));
+//     console.log("complete");
+// console.log(showMultiplicationTable(5));
+//     console.log("complete");
+// console.log(showMultiplicationTable(7));
+//     console.log("complete");
+
+function randomNumber() {
+    for (let i = 0; i < 10; i++) {
+        let randoNum = Math.floor(Math.random() * (201 - 20) + 20);
+        console.log(randoNum % 2 === 0 ? randoNum + " is even" : randoNum + " is odd");
+    }
+}
+
+// console.log(randomNumber());
+
+// let number = "";
+// function pyramid() {
+//     for (let i = 1; i <= 9; i++) {
+//         number = i;
+//         let string = number.toString();
+//         for (let j = 2; j <= i; j++) {
+//             string += number;
+//         }
+//         console.log(string);
+//         }
+//
+//     }
+
+function pyramid() {
+    for (let i = 1; i <= 9; i++) {
+        let number = "";
+        for (let j = 1; j <= i; j++) {
+            // number += i;
+            number = number + i;
+        }
+        console.log(number);
+    }
+}
+
+// function pyramid() {
+//     for (let i = 1; i <= 9; i++) {
+//         console.log(i.toString().repeat(i));
+//     }
+// }
+
+// console.log(pyramid());
+
+function minusFive() {
+    for (let i = 100; i > 5; i -= 5) {
+        console.log(i);
+    }
+}
+
+// console.log(minusFive());
+
