5cdf053d4376183fd5e73486c099c5ae1f5ffd3d
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-10-14 14:23:45 -0500

    finished the first part of the planets exercise

--- js/planets-array.js
+++ js/planets-array.js
@@ -1,7 +1,7 @@
 (function(){
     "use strict";
 
-    var planets = [
+    let planets = [
         'Mercury',
         'Venus',
         'Earth',
@@ -19,23 +19,30 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift('The Sun');
     console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push('Pluto');
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift('The Sun');
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop('Pluto');
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log("The index of Earth is: " + planets.indexOf('Earth'));
 
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
 
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
 
 })();
