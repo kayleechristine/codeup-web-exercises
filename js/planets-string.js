7310e2e2c2fc836faaed6a4b3aacb03a3eb21060
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-10-14 15:03:30 -0500

    finished the planets strings exercise

--- js/planets-string.js
+++ js/planets-string.js
@@ -11,7 +11,9 @@
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
+    planetsArray = planetsString.split("|");
+
+    console.log(planetsArray);
 
     /**
      * TODO:
@@ -23,6 +25,21 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+    planetsString = planetsArray.join("<br>");
+
+    console.log(planetsString);
+
+    /** This could be useful for using JavaScript to modify the formatting of
+     * an HTML page without altering the actual HTML. */
+
+    console.log(planetsArray);
+    let planetsList = planetsArray.join("</li><li>");
+    planetsArray = planetsList.split("");
+    planetsArray.unshift("<ul><li>");
+    planetsArray.push("</li></ul>");
+    planetsList = planetsArray.join("");
+    console.log(planetsList);
 
 })();
 
