dbdc6dcb8496c7420fea933210a3f3f4750f583d
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-11-30 11:39:41 -0600

    weather updates by current location

--- js/weather.js
+++ js/weather.js
@@ -29,117 +29,133 @@ let wxPics = [
     {name: 'Mist', img: 'img/wx/mist.png'} // TODO: Make default image
 ];
 
+function titleCase(str) {
+    let splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(' ');
+}
+
+document.write(titleCase("I'm a little tea pot"));
+
+function updateWeather(lat, lon) {
+
 // Current Day Weather
-$.get("http://api.openweathermap.org/data/2.5/weather", {
-    APPID: OPEN_WEATHER_APPID,
-    q:     "Dallas, US", // TODO: Make variable by location
-    units: "imperial"
-}).done(function(data) {
-    // console.log('Current Weather', data);
-
-    // Day of the Week
-    let date = new Date(data.dt * 1000); // Local Time
-    let day = weekday[date.getDay()].name; // Day of the Week
-
-    // Temperatures
-    let low = Math.round(data.main.temp_min) + '°F'; // Low Temp
-    let high = Math.round(data.main.temp_max) + '°F'; // High Temp
-
-    // Details
-    let img = data.weather[0].description.replace(' ', '-'); // Image
-    let wx = data.weather[0].main; // Wx Description
-    let humid = data.main.humidity; // Humidity
-    let wind = data.wind.speed; // Wind Speed
-    let press = data.main.pressure; // Pressure
-
-    // Pushes to the Card
-    $('#img-1').html(`<img src="img/wx/${img}.png" class="card-img-top">`);
-    $('#day-1').html(day);
-    $('#low-1').html(low);
-    $('#high-1').html(high);
-    $('#wx-1').html(wx); // TODO: More descriptive weather forecast
-    $('#humid-1').html(humid);
-    $('#wind-1').html(wind);
-    $('#press-1').html(press);
-
-});
-
-// 5-day Forecast Test
-$.get("http://api.openweathermap.org/data/2.5/forecast", {
-    APPID: OPEN_WEATHER_APPID,
-    lat:    32.806657,
-    lon:   -97.054590, // TODO: Make variable by location
-    units: "imperial",
-}).done(function(data) {
-    console.log('5 Day Forecast', data);
-
-    // Convert UTC time to Local Time
-    let date = new Date(data.list[0].dt * 1000);
-    let hours = date.getHours();
-
-    // Find the Next Day
-    let offset = date.getTimezoneOffset() / 60; // # hours difference from local to UTC time
-    let index = (((24 - offset) - hours) / 3) + 1; // Index of the next day's first weather report
-
-    // Days Two - Five
-    for (let i = 2; i <= 5; i++) {
+    $.get("http://api.openweathermap.org/data/2.5/weather", {
+        APPID: OPEN_WEATHER_APPID,
+        lat: lat,
+        lon: lon,
+        units: "imperial"
+    }).done(function (data) {
+        // console.log('Current Weather', data);
 
         // Day of the Week
-        let num = new Date(data.list[index + 1].dt * 1000); // Local Time
-        let day = weekday[num.getDay()].name; // Day of the Week
-        console.log(num.getDay());
+        let date = new Date(data.dt * 1000); // Local Time
+        let day = weekday[date.getDay()].name; // Day of the Week
 
-        // Average Daily Temperatures // TODO: Calculate high & low temps
-        let max = index + 8;
-        let tempData = [];
-        for (let i = index; i < max; i++) {
-            tempData.push(data.list[i].main.temp);
-        }
-
-        // function highAndLow(numbers) {
-        //     numbers = numbers.split(" ");
-        //     return Math.max(...numbers) + " " + Math.min(...numbers);
-        // }
-
-        // Calculating Daily Averages
-        let pressureData = [];
-        for (let i = index; i < max; i++) {
-            pressureData.push(data.list[i].main.pressure);
-        }
-        let humidityData = [];
-        for (let i = index; i < max; i++) {
-            humidityData.push(data.list[i].main.humidity);
-        }
-        let windData = [];
-        for (let i = index; i < max; i++) {
-            windData.push(data.list[i].wind.speed);
-        }
-        function findAverage(arr) {
-            return arr.filter(num => num >= 0).reduce((sum, add) => sum + add, 0) / 8;
-        }
+        // Temperatures
+        let low = Math.round(data.main.temp_min) + '°F'; // Low Temp
+        let high = Math.round(data.main.temp_max) + '°F'; // High Temp
 
         // Details
-        let img = data.list[index].weather[0].description.replace(' ', '-'); // Image
-        let wx = data.list[index].weather[0].main; // Wx Description
-        let humid = findAverage(humidityData); // Humidity
-        let wind = findAverage(windData); // Wind Speed
-        let press = findAverage(pressureData); // Pressure
-        // console.log('Index:', index, 'Wx:', img);
+        let img = data.weather[0].description.replace(' ', '-'); // Image
+        let wx = data.weather[0].description; // Wx Description
+        let humid = data.main.humidity; // Humidity
+        let wind = data.wind.speed; // Wind Speed
+        let press = data.main.pressure; // Pressure
 
         // Pushes to the Card
-        $(`#img-${i}`).html(`<img src="img/wx/${img}.png" class="card-img-top">`);
-        $(`#day-${i}`).html(day);
-        $(`#low-${i}`).html(Math.round(Math.min(...tempData)) + '°F');
-        $(`#high-${i}`).html(Math.round(Math.max(...tempData)) + '°F');
-        $(`#wx-${i}`).html(wx); // TODO: More descriptive weather forecast
-        $(`#humid-${i}`).html(Math.round(humid));
-        $(`#wind-${i}`).html(wind.toFixed(2));
-        $(`#press-${i}`).html(Math.round(press));
+        $('#img-1').html(`<img src="img/wx/${img}.png" class="card-img-top">`);
+        $('#day-1').html(day);
+        $('#low-1').html(low);
+        $('#high-1').html(high);
+        $('#wx-1').html(titleCase(wx));
+        $('#humid-1').html(humid);
+        $('#wind-1').html(wind);
+        $('#press-1').html(press);
 
-        index += 8;
+    });
 
-    }
+// 5-day Forecast Test
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: OPEN_WEATHER_APPID,
+        lat: lat,
+        lon: lon,
+        units: "imperial",
+    }).done(function (data) {
+        // console.log('5 Day Forecast', data);
 
+        // Convert UTC time to Local Time
+        let date = new Date(data.list[0].dt * 1000);
+        let hours = date.getHours();
+
+        // Find the Next Day
+        let offset = date.getTimezoneOffset() / 60; // # hours difference from local to UTC time
+        let index = (((24 - offset) - hours) / 3) + 1; // Index of the next day's first weather report
+
+        // Days Two - Five
+        for (let i = 2; i <= 5; i++) {
+
+            // Day of the Week
+            let num = new Date(data.list[index + 1].dt * 1000); // Local Time
+            let day = weekday[num.getDay()].name; // Day of the Week
+
+            // Average Daily Temperatures
+            let max = index + 8;
+            let tempData = [];
+            for (let i = index; i < max; i++) {
+                tempData.push(data.list[i].main.temp);
+            }
+
+            // Calculating Daily Averages
+            let pressureData = [];
+            for (let i = index; i < max; i++) {
+                pressureData.push(data.list[i].main.pressure);
+            }
+            let humidityData = [];
+            for (let i = index; i < max; i++) {
+                humidityData.push(data.list[i].main.humidity);
+            }
+            let windData = [];
+            for (let i = index; i < max; i++) {
+                windData.push(data.list[i].wind.speed);
+            }
+
+            function findAverage(arr) {
+                return arr.filter(num => num >= 0).reduce((sum, add) => sum + add, 0) / 8;
+            }
+
+            // Details
+            let img = data.list[index].weather[0].description.replace(' ', '-'); // Image
+            let wx = data.list[index].weather[0].description; // Wx Description
+            let humid = findAverage(humidityData); // Humidity
+            let wind = findAverage(windData); // Wind Speed
+            let press = findAverage(pressureData); // Pressure
+
+            // Pushes to the Card
+            $(`#img-${i}`).html(`<img src="img/wx/${img}.png" class="card-img-top">`);
+            $(`#day-${i}`).html(day);
+            $(`#low-${i}`).html(Math.round(Math.min(...tempData)) + '°F');
+            $(`#high-${i}`).html(Math.round(Math.max(...tempData)) + '°F');
+            $(`#wx-${i}`).html(titleCase(wx));
+            $(`#humid-${i}`).html(Math.round(humid));
+            $(`#wind-${i}`).html(wind.toFixed(2));
+            $(`#press-${i}`).html(Math.round(press));
+
+            index += 8;
+
+        }
+    });
+
+}
+
+// Loads Dallas Weather by Default
+window.addEventListener("load", (event) => {
+    updateWeather(32.806657, -95.054590);
 });
 
 // Creates the Map
@@ -180,60 +196,52 @@ map.addControl(
     })
 );
 
-// Radar API
-// map.addSource("twcRadar", {
-//     type: "raster",
-//     tiles: [
-//         "https://api.weather.com/v3/TileServer/tile/radar?s=" + lastTimeSlice + "&xyz={x}:{y}:{z}&apiKey=" + twcApiKey,
-//     ],
-//     tileSize: 256,
-// });
-//
-// map.addLayer(
-//     {
-//         id: "radar",
-//         type: "raster",
-//         source: "twcRadar",
-//         paint: {
-//             "raster-opacity": 0.5,
-//         },
-//     },
-//     "aeroway-line"
-// );
+// Update Weather by Location
+$('.mapboxgl-ctrl-geocoder--input').change(function() {
+
+    let location = $(this).val();
+
+    $.get("http://api.openweathermap.org/geo/1.0/direct", {
+        APPID: OPEN_WEATHER_APPID,
+        q: location
+    }).done(function(data) {
+
+        let lat = data[0].lat;
+        let lon = data[0].lon;
+        updateWeather(lat, lon);
+    })
+});
+// TODO: Update by Current Location
+
+console.log('Mapbox:', map);
 
 // Saved Locations
 // TODO: Allow user to save locations for later
-// let savedLocations = [
-//     {
-//         name: "San Diego",
-//         location: [-96.780709, 32.784360],
-//         weather: "Sunny",
-//         image: "img/breakfast.png"
-//     },
-//     {
-//         name: "Monterey",
-//         location: [-96.784189, 32.784003],
-//         type: "Overcast",
-//         image: "img/taco.png"
-//     },
-//     {
-//         name: "Dallas",
-//         location: [-96.784171, 32.784795],
-//         type: "Rainy",
-//         image: "img/chicken.png"
-//     }
-// ];
+let savedLocations = [
+    {
+        name: "San Diego",
+        location: [-110.163948, 32.742266],
+        weather: "Sunny",
+        image: "img/breakfast.png"
+    },
+    {
+        name: "Dallas",
+        location: [-96.784171, 32.784795],
+        type: "Rainy",
+        image: "img/chicken.png"
+    }
+];
 
 // Generates Markers & Popups
-// let marker;
-// savedLocations.forEach(function(location){
-//     let popup = new mapboxgl.Popup()
-//         .setHTML(`<div class="popup"><img src="${location.image}" style="width: 50px; margin: auto;"><h3>${location.name}</h3> <p>${location.type}</p></div>`);
-//     marker = new mapboxgl.Marker()
-//         .setLngLat(location.location)
-//         .addTo(map)
-//         .setPopup(popup);
-// });
+let marker;
+savedLocations.forEach(function(location){
+    let popup = new mapboxgl.Popup()
+        .setHTML(`<div class="popup"><img src="${location.image}" style="width: 50px; margin: auto;"><h3>${location.name}</h3> <p>${location.type}</p></div>`);
+    marker = new mapboxgl.Marker()
+        .setLngLat(location.location)
+        .addTo(map)
+        .setPopup(popup);
+});
 
 // ******************************************************************************************************************//
 
