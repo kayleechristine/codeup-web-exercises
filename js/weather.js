80d22f8ce845f05c74e034c0c1e45116a65da9b5
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-11-30 19:55:50 -0600

    finished cleaning up the code

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,7 @@
 "use strict";
 
 // API Key
-const MAPBOX_TOKEN = "pk.eyJ1Ijoia2F5bGVlY2hyaXN0aW5lIiwiYSI6ImNsYXF4dzVkbDFvMmkzb3Q3a3hvbjY5eWwifQ.kNDNcmM_XjUow6-di1yyTw";
-const OPEN_WEATHER_APPID = "f0817a4ce29f6dc633fb41e280415439";
+
 
 // Days of the Week
 let weekday = [
